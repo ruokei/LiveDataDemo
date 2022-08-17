43caee247ab3ae394ad3bb7a5a25a90e9f5e10ea
anreh <anurehka@gmail.com>
2022-08-17 11:49:23 +0800

    y

--- app/src/main/java/com/example/livedatademo/AccountModel.kt
+++ app/src/main/java/com/example/livedatademo/AccountModel.kt
@@ -1,24 +1,35 @@
 package com.example.livedatademo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class AccountModel(accNo:String, owner:String, balance:Double) {
-    var accNo : String = ""
+class AccountModel(accNo:String="", owner:String="", balance:Double=0.0): ViewModel(){
+    private var accNo : String = ""
+    var accountNo : String
+        get() = accNo
+        set(value){ accNo = value}
+
     var owner : String = ""
-    var balance: Double =0.0
+
+    private var balance = MutableLiveData<Double>()
+    var accBalance : MutableLiveData<Double>
+        get() = balance
+        set(value){ balance = value}
 
     init {
         this.accNo = accNo
         this.owner = owner
-        this.balance = balance
+        this.balance.value = balance
     }
 
     fun deposit(amount: Double) {
-        balance += amount
+        balance.value = balance.value?.plus(amount)
     }
 
     fun withdraw(amount:Double){
-        balance -= amount
+        balance.value = balance.value?.minus(amount)
     }
+
 
 }
--- app/src/main/java/com/example/livedatademo/MainActivity.kt
+++ app/src/main/java/com/example/livedatademo/MainActivity.kt
@@ -1,16 +1,52 @@
 package com.example.livedatademo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.livedatademo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var myAccountModel : AccountModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  DataBindingUtil.setContentView(this, R.layout.activity_main)
-    
+
+        // myAccountModel = AccountModel("A123", "John", 100.0)
+        myAccountModel = ViewModelProvider(this).get(AccountModel::class.java)
+
+        if(myAccountModel.owner == "") {
+            myAccountModel.owner = "John"
+            myAccountModel.accountNo = "A123"
+            myAccountModel.accBalance.value = 100.0
+        }
+        display()
+
+        myAccountModel.accBalance.observe(this, Observer{
+            newBalance -> binding.tvBalance.text = newBalance.toString()
+        })
+        binding.btnDeposit.setOnClickListener(){
+            val amount:Double = binding.tfAmount.text.toString().toDouble()
+            myAccountModel.deposit(amount)
+            display()
+
+        }
+
+        binding.btnWitdraw.setOnClickListener(){
+            val amount:Double = binding.tfAmount.text.toString().toDouble()
+            myAccountModel.withdraw(amount)
+            display()
+        }
     }
+
+    private fun display(){
+        binding.tvName.text = myAccountModel.owner
+        binding.tvAccNo.text = myAccountModel.accountNo
+        binding.tvBalance.text = myAccountModel.accBalance.value.toString()
+    }
+
 }
